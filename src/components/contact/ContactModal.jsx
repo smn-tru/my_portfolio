ce6e4ad4db1d81040e465dc988c4842c1953516c
Sandra Truong <smntru@Sandras-MacBook-Pro.local>
2023-06-04 22:28:52 -0600

    added validation to emailForm in contactModal

--- src/components/contact/ContactModal.jsx
+++ src/components/contact/ContactModal.jsx
@@ -1,12 +1,41 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
-import PropTypes from 'prop-types';
+import React, { useState } from "react";
+//import PropTypes from 'prop-types';
+import * as Yup from "yup";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 import "./contact.css"
 
 function ContactModal(){
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: "",
+    });
 
+    console.log(setFormData);
+
+    const submitHandler = () => {
+        console.log("submitHandler clicked")
+    };
+
+    const changeHandler = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevState) => {
+            let data = { ...prevState };
+            data[name] = value;
+            return data;
+        });
+    };
+
+    const emailFormSchema = Yup.object().shape({
+        name: Yup.string().required('Required'),
+        email: Yup.string().email('Invalid email').required('Required'),
+        subject: Yup.string().required('Required'),
+        message: Yup.string().required('Required'),
+    });
 
     return(
        <>
@@ -16,7 +45,7 @@ function ContactModal(){
                 <div className="modal-header text-center">
                     <h4 className="modal-title w-100 font-weight-bold">Contact</h4>
                     <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true"><i className="fa-regular fa-circle-xmark" /></span>
+                        <span className="close-modal" aria-hidden="true"><i className="fa-regular fa-circle-xmark" /></span>
                     </button>
                 </div>
                 <div className="modal-body mx-3">
@@ -81,30 +110,46 @@ function ContactModal(){
                         <div className="col-9 ml-4">
                         <div className="tab-content" id="tabContent">
                         <div className="tab-pane fade show active" id="emailForm" role="tabpanel">
-                            <div className="emailForm" display="block">
-                                <div className="md-form mb-2">
-                                    <input type="text" id="form34" className="form-control validate" placeholder="name" />
-                                    <label data-error="wrong" data-success="right" htmlFor="form34"></label>
-                                </div>
+                            <Formik 
+                                enableReinitialize={true}
+                                initialValues={formData}
+                                onSubmit={submitHandler}
+                                validationSchema={emailFormSchema}>
+                                    <Form className="emailForm">
+                                        <div className="md-form mb-2">
+                                            <Field type="text" id="nameField" name="name" value={formData.name} className="form-control validate" placeholder="name" onChange={changeHandler} />
+                                            <label className="validate-field">
+                                                <ErrorMessage name="name" component="div" className="has-error" />
+                                            </label>
+                                           
+                                        </div>
+                                    
+                                        <div className="md-form mb-2">
+                                            <Field type="email" id="emailField" name="email" value={formData.email} className="form-control validate" placeholder="email" onChange={changeHandler}/>
+                                            <label className="validate-field">
+                                                <ErrorMessage name="email" component="div" className="has-error" />
+                                            </label>
+                                        </div>
 
-                                <div className="md-form mb-2">
-                                    <input type="email" id="form29" className="form-control validate" placeholder="email"/>
-                                    <label data-error="wrong" data-success="right" htmlFor="form29"></label>
-                                </div>
+                                        <div className="md-form mb-2">
+                                            <Field type="text" id="subjectField" name="subject" value={formData.subject} className="form-control validate" placeholder="subject" onChange={changeHandler} />
+                                            <label className="validate-field">
+                                                <ErrorMessage name="subject" component="div" className="has-error" />
+                                            </label>
+                                        </div>
 
-                                <div className="md-form mb-2">
-                                    <input type="text" id="form32" className="form-control validate" placeholder="subject" />
-                                    <label data-error="wrong" data-success="right" htmlFor="form32"></label>
-                                </div>
-
-                                <div className="md-form">
-                                    <textarea type="text" id="form8" className="md-textarea form-control" rows="4" placeholder="message"></textarea>
-                                    <label data-error="wrong" data-success="right" htmlFor="form8"></label>
-                                </div>
-                                <div className="modal-footer d-flex justify-content-center">
-                                    <button type="submit" className="btn sendForm">Send</button>
-                                </div>
-                            </div>
+                                        <div className="md-form">
+                                            <textarea type="textarea" id="messageField" name="message" value={formData.message} className="md-textarea form-control" rows="4" placeholder="message" onChange={changeHandler}></textarea>
+                                            <label className="validate-field">
+                                                <ErrorMessage name="message" component="div" className="has-error" />
+                                            </label>
+                                        </div>
+                                        <div className="modal-footer d-flex justify-content-center">
+                                            <button type="submit" className="btn sendForm">Send</button>
+                                        </div>
+                                    </Form>
+                                
+                            </Formik>
                         </div>
 
                         <div className="tab-pane fade" id="slackInfoCard" role="tabpanel">
@@ -148,10 +193,6 @@ function ContactModal(){
         </div>
        </>
     )
-}
-
-ContactModal.propTypes = {
-    toggleVisibility: PropTypes.func,
 }
 
 export default ContactModal;
