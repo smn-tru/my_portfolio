44c9630d143d6a30e15cb2129373e3e058c8ecdc
Sandra Truong <smntru@Sandras-MacBook-Pro.local>
2023-06-11 17:59:56 -0600

    skillsModal complete

--- src/components/skills/SkillsModal.jsx
+++ src/components/skills/SkillsModal.jsx
@@ -1,31 +1,22 @@
 /* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
-import './skills.css'
+import { Modal } from 'bootstrap';
+import './skills.css';
+
+import ResumeModal from "../resume/ResumeModal";
 
 function SkillsModal(){
 
-    let box = document.querySelector("skills-container");
-
-    box?.addEventListener("click", function(){
-        box.classList.toggle("active");
-    })
-
-    const keyFunction = e => {
-        if (e.keyCode === 38){
-            box.classList.add("active");
-        }
-        if (e.keyCode === 40){
-            box.classList.remove("active");
-        }
-    };
-    
-
+    const onViewResumeClick = () => {
+        const resumeModal = new Modal("#resumeModal");
+        resumeModal.show();
+    }
 
     return (
+        <>
         <div className="modal fade" id="skillsModal" tabIndex="-1" role="dialog" aria-labelledby="skillsModal" aria-hidden="true">
             <div className="modal-dialog" role="document">
-
                 <div className="modal-content">
                     <div className="modal-header text-center">
                         <h4 className="modal-title w-100 font-weight-bold">Skills</h4>
@@ -33,8 +24,7 @@ function SkillsModal(){
                             <span className="close-modal" aria-hidden="true"><i className="fa-regular fa-circle-xmark fa-xl" /></span>
                         </a>
                     </div>
-
-                    <div className="modal-body skills-body mx-3" onKeyDown={keyFunction}>
+                    <div className="modal-body skills-body mx-3">
                         <div className="row">
                             <div className="flip-card">
                                 <div className="flip-card-inner">
@@ -42,7 +32,17 @@ function SkillsModal(){
                                         <h3>FRONT END</h3>
                                     </div>
                                     <div className="flip-card-back">
-                                        <h3>Whoaaa!!!</h3>
+                                        <ul>
+                                            <li>AJAX</li>
+                                            <li>Bootstrap</li>
+                                            <li>CSS</li>
+                                            <li>HTML</li>
+                                            <li>JavaScript</li>
+                                            <li>jQuery</li>
+                                            <li>Markdown</li>
+                                            <li>React / REACT.js</li>
+                                            <li>RDL/XML</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
@@ -52,7 +52,14 @@ function SkillsModal(){
                                         <h3>BACK END</h3>
                                     </div>
                                     <div className="flip-card-back">
-                                        <h3>Whoaaa!!!</h3>
+                                        <ul>
+                                            <li>ASP.NET Core</li>
+                                            <li>ASP.NET MVC</li>
+                                            <li>C#</li>
+                                            <li>REST API</li>
+                                            <li>SQL / T-SQL</li>
+                                            <li>Web Forms / WinForms</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
@@ -61,10 +68,16 @@ function SkillsModal(){
                             <div className="flip-card">
                                 <div className="flip-card-inner">
                                     <div className="flip-card-front">
-                                        <h3>MIDDLE TIER</h3>
+                                        <h3>MIDDLEWARE</h3>
                                     </div>
                                     <div className="flip-card-back">
-                                        <h3>Whoaaa!!!</h3>
+                                        <ul>
+                                            <li>ADO.NET</li>
+                                            <li>ASP.NET Core</li>
+                                            <li>Azure</li>
+                                            <li>C#</li>
+                                            <li>Docker</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
@@ -74,16 +87,32 @@ function SkillsModal(){
                                         <h3>TOOLS</h3>
                                     </div>
                                     <div className="flip-card-back">
-                                        <h3>Whoaaa!!!</h3>
+                                        <ul>
+                                            <li>AGILE / SCRUM</li>
+                                            <li>Chrome DevTools</li>
+                                            <li>Git</li>
+                                            <li>Github</li>
+                                            <li>Microsoft Visual Studios</li>
+                                            <li>Microsoft SQL Server</li>
+                                            <li>PowerShell</li>
+                                            <li>Spiceworks</li>
+                                            <li>Visual Studio Code</li>
+                                            <li>VS Debugger / Unit Test</li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    <div className="modal-footer d-flex justify-content-center mt-3"></div>
+                    <div className="modal-footer d-flex justify-content-center mt-3 p-0"></div>
+                    </div>
+                    <div className="col">
+                        <button type="button" className="btn viewResume mb-3 me-5 mt-0 float-end" onClick={onViewResumeClick} data-bs-dismiss="modal">&nbsp;View Resume</button>
                     </div>
                 </div>
             </div>
         </div>
+        <ResumeModal />
+        </>
     )
 }
 
