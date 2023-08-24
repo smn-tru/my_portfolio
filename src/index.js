7dbba5046fa4d778a2ce530307d955baa96fece5
Sandra Truong <sandrat1790@gmail.com>
2023-08-23 19:45:43 -0600

    setup gh-pages

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+//import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+//import './index.css';
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<React.StrictMode><App /></React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
